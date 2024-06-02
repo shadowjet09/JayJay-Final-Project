e2f2b05bf09851ae15cf5235fe4b3cbd42d619ee
DaffaPintu <daffa@pintu.co.id>
2024-06-02 10:56:50 +0700

    FIX

--- src/test/java/helper/Models.java
+++ src/test/java/helper/Models.java
@@ -10,9 +10,9 @@ public class Models {
 
     public static void setupHeaders() {
         request =  RestAssured.given()
-                .header(headerName: "Content-type", headerValue: "application/json")
-                .header(headerName: "Accept", headerValue: "application/json")
-                .header(headerName: "Authorization", headerValue: "Bearer 276fffa632f72e79f84ddb103fa6531ad16d45e0c899ef11ca1fcac722071290");
+                .header("Content-type","application/json")
+                .header("Accept","application/json")
+                .header("Authorization","Bearer 276fffa632f72e79f84ddb103fa6531ad16d45e0c899ef11ca1fcac722071290");
     }
 
     public static Response getListUsers(String endpoint) {
